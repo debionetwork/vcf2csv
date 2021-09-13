cd6319f32cba8e278b3e35668f8d6599831055df
Agustinus Theodorus <agustinus.theodorus@binus.edu>
2021-09-13 09:32:11 +0700

    fix: broken csv transformation

--- main.py
+++ main.py
@@ -3,8 +3,18 @@ vcfContents = file.readlines()[5:] # Ignore first 5 lines
 file.close()
 
 import re
-for vcfContent in vcfContents:
-  vcfContent = vcfContent.replace(',', '","')
-  csvContent = re.sub(r'\t', ',', vcfContent)
+for csvContent in vcfContents:
+  # Turn all starting characters to "
+  csvContent = re.sub(r'^', '"', csvContent)
+  # Turn all ending characters to "
+  csvContent = re.sub(r'\n', '', csvContent)
+  # Turn all ending characters to "
+  csvContent = re.sub(r'\t$', '"', csvContent)
+  if not re.search(r'"$', csvContent):
+    # Turn all ending characters to "
+    csvContent = re.sub(r'$', '"', csvContent)
+  # Turn all whitespaces to ","
+  csvContent = re.sub(r'\t', '","', csvContent)
   with open("testFile_With_Scores.csv", "a") as csvFile:
     csvFile.write(csvContent)
+    csvFile.write('\n')
